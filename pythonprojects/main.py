304b7751b49cbf515c179bf47cfe50aaa6ce0647
Anastasia Lukacsi <kstasyl@gmail.com>
2025-07-11 15:37:26 +0300

    Update main.py

--- pythonprojects/main.py
+++ pythonprojects/main.py
@@ -1,10 +1,10 @@
 import requests
 URL = 'https://api.pokemonbattle.ru/v2'
-TOKEN = '822e8149e9407def35ad6f621b2da961'
+TOKEN = 'токен тренера'
 HEADER = {"Content-Type":"application/json", "trainer_token":TOKEN} 
 #body_create = {
-    #"name": "Лабубу",
-    #"photo_id": 909
+    #"name": "имя покемона",
+    #"photo_id": номер картинки
 #}
 
 #response_create = requests.post(url=f'{URL}/pokemons', headers=HEADER, json=body_create)
@@ -15,16 +15,16 @@ HEADER = {"Content-Type":"application/json", "trainer_token":TOKEN}
 
 #body_update = {
 
-    #"pokemon_id": "342797",
-    #"name": "Nectar",
-    #"photo_id": 909
+    #"pokemon_id": "айди покемона",
+    #"name": "новое имя покемона",
+    #"photo_id": номер картинки
 #}
 
 #response_update=requests.put(url=f'{URL}/pokemons', headers=HEADER, json=body_update)
 #print(response_update.json())
 
 body_catch = {
-    "pokemon_id": "342797"
+    "pokemon_id": "айди покемона"
 }
 response_catch=requests.post(url=f'{URL}/trainers/add_pokeball', headers=HEADER, json=body_catch)
 print(response_catch.json())
